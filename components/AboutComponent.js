3db5c2b4ecba9d94dfc2f4b3c8367529a331e667
MarinaGamal <marina.gamal.elias@hotmail.com>
2020-05-07 17:10:49 +0200

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { Text, FlatList, View, ScrollView , StyleSheet} from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { Card } from 'react-native-elements';
-import { ABOUT } from '../shared/about';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-
-
-
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -16,67 +13,77 @@ const mapStateToProps = state => {
     }
   }
 
-function History(props) {
+ class About extends Component {
+     
+
     
-        const item = props.item; 
-            return(
-                <Card
-                    title={item.title}>
-                    <Text
-                        style={{margin: 10}}>
-                        {item.description}</Text>
-                </Card>
-            );
-}
-function RenderItemLeaders(props){
-
-    const renderLeaderItem = ({item, index}) => {
-
-        return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={<Text style={{color:"#808080"}}>{item.description}</Text>}
-                    hideChevron={true}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                />
-
-               
-        );
-    }
-    const item = props.item; 
-            return(
-                <Card
-                    title="Corporate Leadership"
-                >
-                     <FlatList 
-                        data={this.props.leaders.leaders}
-                        renderItem={renderLeader}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            );
-}
-
-
-class About extends Component {
-
     static navigationOptions = {
-        title: 'About Us',
+        title: 'About Us'
     };
 
-    render() {
-       
-        return(
-        <ScrollView>
-            <History item={this.state.about} />
-            <RenderItemLeaders item={this.state.leaders} />
-    
+     render() {
+
+        const renderLeadersItem = ({item, index}) => {
+            return (
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        titleStyle = {{color: 'black',fontWeight: 'bold'}}
+                        subtitle={item.description}
+                        subtitleStyle={{ color: 'grey' }}
+                        hideChevron={true}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                    />
+            );
+        };
+
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+            <ScrollView>
+                <Card title='Our History'>
+                    <Text style={{margin: 5}}>Started in 2010, Ristorante con Fusion quickly established 
+                    itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion 
+                    cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  
+                    Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your 
+                    plate the next time you visit us.</Text>
+                    <Text style={{margin: 5}}>The restaurant traces its humble beginnings to The Frying Pan, 
+                    a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time 
+                    the world's best cuisines in a pan.</Text>     
+                </Card>
+                <Card title='Corporate Leadership'>
+                    <FlatList 
+                        data={this.props.leaders.leaders}
+                        renderItem={renderLeadersItem}
+                        keyExtractor={item => item.id.toString()}
+                        />
+                </Card>
         </ScrollView>
-        );
+            );
+        }
     }
 }
-
 
 
 export default connect(mapStateToProps)(About);
