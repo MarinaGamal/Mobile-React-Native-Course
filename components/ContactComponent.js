1a85978dacf9d084795c5c9c8fec4de4cbfd3c5b
MarinaGamal <marina.gamal.elias@hotmail.com>
2020-05-23 22:31:17 +0200

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Text} from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import {CONTACT} from '../shared/contact';
 import * as Animatable from 'react-native-animatable';
+import { MailComposer } from 'expo';
+
+
+
 
 function RenderItem(props) {
+    
     
         const item = props.item;
         
@@ -15,7 +20,14 @@ function RenderItem(props) {
                         <Text
                             style={{margin: 10}}>
                             {item.details}</Text>
+                        <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: "#512DA8"}}
+                            icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                            onPress={this.sendMail}
+                        />
                     </Card>
+
                 </Animatable.View>
             );
 }
@@ -33,6 +45,14 @@ class ContactUs extends Component {
     static navigationOptions = {
         title: 'Contact Us',
     };
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
 
     render() {
         return(
